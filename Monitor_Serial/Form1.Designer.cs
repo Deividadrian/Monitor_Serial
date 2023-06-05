3d579b7a05e8a4a4b287836760fa52192f280b91
deividadrian <deividadrian24@gmail.com>
2023-06-04 22:58:07 -0300

    Add project files.

---
+++ Monitor_Serial/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace Monitor_Serial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbPortas = new System.Windows.Forms.ComboBox();
+            this.cbBaud = new System.Windows.Forms.ComboBox();
+            this.cbParidade = new System.Windows.Forms.ComboBox();
+            this.cdBitsDados = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cdBitsParadas = new System.Windows.Forms.ComboBox();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.Serial = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtTransmite = new System.Windows.Forms.TextBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Porta:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(125, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Velocidade:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(228, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Paridade:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(332, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Bits de Dados:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(431, 47);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Bit de Parada.:";
+            // 
+            // cbPortas
+            // 
+            this.cbPortas.FormattingEnabled = true;
+            this.cbPortas.Location = new System.Drawing.Point(32, 63);
+            this.cbPortas.Name = "cbPortas";
+            this.cbPortas.Size = new System.Drawing.Size(79, 21);
+            this.cbPortas.TabIndex = 5;
+            this.cbPortas.SelectedIndexChanged += new System.EventHandler(this.cbPortas_SelectedIndexChanged);
+            // 
+            // cbBaud
+            // 
+            this.cbBaud.FormattingEnabled = true;
+            this.cbBaud.Items.AddRange(new object[] {
+            "300",
+            "600",
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "115200"});
+            this.cbBaud.Location = new System.Drawing.Point(128, 63);
+            this.cbBaud.Name = "cbBaud";
+            this.cbBaud.Size = new System.Drawing.Size(79, 21);
+            this.cbBaud.TabIndex = 6;
+            this.cbBaud.SelectedIndexChanged += new System.EventHandler(this.cbBaud_SelectedIndexChanged);
+            // 
+            // cbParidade
+            // 
+            this.cbParidade.FormattingEnabled = true;
+            this.cbParidade.Location = new System.Drawing.Point(231, 63);
+            this.cbParidade.Name = "cbParidade";
+            this.cbParidade.Size = new System.Drawing.Size(79, 21);
+            this.cbParidade.TabIndex = 7;
+            this.cbParidade.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // cdBitsDados
+            // 
+            this.cdBitsDados.FormattingEnabled = true;
+            this.cdBitsDados.Items.AddRange(new object[] {
+            "5",
+            "6",
+            "7",
+            "8"});
+            this.cdBitsDados.Location = new System.Drawing.Point(335, 63);
+            this.cdBitsDados.Name = "cdBitsDados";
+            this.cdBitsDados.Size = new System.Drawing.Size(79, 21);
+            this.cdBitsDados.TabIndex = 8;
+            this.cdBitsDados.SelectedIndexChanged += new System.EventHandler(this.cdBitsDados_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cdBitsParadas);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cdBitsDados);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cbParidade);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.cbBaud);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.cbPortas);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(549, 113);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Configuração";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // cdBitsParadas
+            // 
+            this.cdBitsParadas.FormattingEnabled = true;
+            this.cdBitsParadas.Location = new System.Drawing.Point(434, 63);
+            this.cdBitsParadas.Name = "cdBitsParadas";
+            this.cdBitsParadas.Size = new System.Drawing.Size(79, 21);
+            this.cdBitsParadas.TabIndex = 9;
+            this.cdBitsParadas.SelectedIndexChanged += new System.EventHandler(this.cdBitsParadas_SelectedIndexChanged);
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(567, 25);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(84, 23);
+            this.btnAbrir.TabIndex = 10;
+            this.btnAbrir.Text = "Abrir Porta";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(567, 54);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(84, 23);
+            this.btnFechar.TabIndex = 11;
+            this.btnFechar.Text = "Fechar Porta";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(567, 83);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(84, 23);
+            this.btnSair.TabIndex = 10;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnEnviar);
+            this.groupBox2.Controls.Add(this.txtTransmite);
+            this.groupBox2.Location = new System.Drawing.Point(12, 140);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(549, 100);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Transmissão";
+            // 
+            // txtTransmite
+            // 
+            this.txtTransmite.Location = new System.Drawing.Point(32, 32);
+            this.txtTransmite.Name = "txtTransmite";
+            this.txtTransmite.Size = new System.Drawing.Size(481, 20);
+            this.txtTransmite.TabIndex = 0;
+            this.txtTransmite.TextChanged += new System.EventHandler(this.txtTransmite_TextChanged);
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(438, 58);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(75, 23);
+            this.btnEnviar.TabIndex = 1;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 275);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cbPortas;
+        private System.Windows.Forms.ComboBox cbBaud;
+        private System.Windows.Forms.ComboBox cbParidade;
+        private System.Windows.Forms.ComboBox cdBitsDados;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cdBitsParadas;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Button btnSair;
+        private System.IO.Ports.SerialPort Serial;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.TextBox txtTransmite;
+    }
+}
+
---
+++ Monitor_Serial/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Monitor_Serial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+        private void atualizaCom()
+        {
+            //Limpa todos os items em cbPortas caso existam
+            cbPortas.Items.Clear();
+            /* Para cada nome de porta de comunicação serial indentificado
+             * será atribuido á variável 's' */
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                //adiciona a variável  's' a cada item de cbPortas
+                cbPortas.Items.Add(s);
+            }
+            //Seleciona o item de índice 0 em cbPortas
+            cbPortas.SelectedIndex = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            atualizaCom();
+
+            cbBaud.SelectedText = "9600";
+
+            paridade();
+
+            cdBitsDados.SelectedText = "8";
+
+            bitsParadas();
+
+            btnFechar.Enabled = false;
+            btnAbrir.Enabled = true;
+            btnSair.Enabled = true;
+        }
+
+        private void cbPortas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void paridade()
+        {
+            int i = 0;
+
+            //Limpa todos os items em cbParidade caso exixtam
+            cbParidade.Items.Clear();
+
+            //Para cada nome de paridade identificado será atribuido á variavel 's' 
+            foreach (string s in Enum.GetNames(typeof(Parity)))
+            {
+                cbParidade.Items.Add(s);
+
+                /*verifica se o nome recebido é 'None',
+                 * caso seja, seleciona o seu índice para exibição
+                 */
+                if (s == "None")
+                    cbParidade.SelectedIndex = i;
+
+                i++;  //incrementa a variável i
+            }
+        }
+
+        private void bitsParadas()
+        {
+            int i = 0;
+
+            cdBitsParadas.Items.Clear();
+
+            foreach(string str in Enum.GetNames(typeof(StopBits)))
+            {
+                cdBitsParadas.Items.Add(str);
+
+                if (str == "One")
+                    cdBitsParadas.SelectedIndex = i;
+
+                i++;
+            }
+        }
+
+        private void cdBitsDados_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbBaud_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cdBitsParadas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            if (Serial.IsOpen == true) 
+                Serial.Close();
+
+            Serial.PortName = cbPortas.Text;
+            Serial.BaudRate = Int32.Parse(cbBaud.Text);
+            Serial.Parity = (Parity)cbParidade.SelectedIndex;
+            Serial.DataBits = Int32.Parse(cdBitsDados.Text);
+            Serial.StopBits = (StopBits)cdBitsParadas.SelectedIndex;
+
+            try
+            {
+                Serial.Open();
+                btnAbrir.Enabled = false;
+                btnFechar.Enabled = true;
+                btnFechar.Enabled = false;
+            }
+            catch
+            {
+                MessageBox.Show("Não foi possivel abrir a porta selecionada",
+                    "ATENÇÂO",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+
+                btnFechar.Enabled = false;
+                btnAbrir.Enabled = true;
+                btnSair.Enabled = false;
+            }
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Serial.Close();
+            btnFechar.Enabled = false;
+            btnSair.Enabled = true;
+            btnSair.Enabled = true;
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            if(Serial.IsOpen)
+            {
+                Serial.Write(txtTransmite.Text);
+            }
+        }
+
+        private void txtTransmite_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
